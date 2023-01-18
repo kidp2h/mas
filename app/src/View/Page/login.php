483c61b01ed1071dfa7116b9bf6d104d75bdcae0
kidp2h <f7kingeagles@gmail.com>
2023-01-18 16:45:34 +0700

    mas report

--- app/src/View/Page/login.php
+++ app/src/View/Page/login.php
@@ -51,48 +51,6 @@ $this->layout('auth'); ?>
     </div>
   </div>
 
-  <!-- <table>
-    <tr class="groupInput">
-      <td class="fieldInput empty"></td>
-      <td><span class="titleForm">Memory Album System</span></td>
-    </tr>
-
-
-    <tr class="groupInput">
-      <td class="fieldInput"><label for="email">E-mail</label></td>
-      <td class="colInput">
-        <?php if (isset($form['email'])) { ?>
-
-          <span class="message"><?= $form['email'][0] ?></span>
-        <?php } ?>
-        <?php if (isset($message)) { ?>
-
-          <span class="messageResponse"><?= $message ?></span>
-        <?php } ?>
-        <input type="email" name="email" placeholder="E-mail" />
-      </td>
-    </tr>
-    <tr class="groupInput">
-      <td class="fieldInput"><label for="password">Password</label></td>
-      <td class="colInput">
-        <?php if (isset($form['password'])) { ?>
-
-          <span class="message"><?= $form['password'][0] ?></span>
-        <?php } ?>
-
-        <input type="password" name="password" placeholder="Password" min="8" />
-      </td>
-    </tr>
-    <tr class="groupInput">
-      <td class="fieldInput empty"></td>
-      <td><button type="submit">Login</button></td>
-    </tr>
-    <tr class="groupInput linkRedirect">
-      <td class="fieldInput empty"></td>
-      <td><a href="/user/register">Sign Up</a></td>
-    </tr>
-  </table> -->
-
 
 </form>
 <?php $this->end(); ?>
